d2c0ebc02b40bbed2df57203ccc35ada49f6ee8b
forLoop94 <charlesuwem94@gmail.com>
2022-12-19 22:37:21 +0100

    add playOneRound function to test player and computer playing one round

--- app.js
+++ app.js
@@ -2,15 +2,36 @@ console.log("hello worl!");
 function getComputerChoice() {
     let randomInteger = getRndInteger(1, 4);
     let computerChoice = (randomInteger === 1) ? "rock" : (randomInteger === 2) ? "paper" : (randomInteger === 3) ? "scissors" : "";
+    console.log(computerChoice);
     return computerChoice;
 }
-
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function playOneRound(playerSelection, computerSelection) {
+    let result;
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        result = "computer wins!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        result = "you win!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        result = "computer wins!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        result = "you win!"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        result = "you win!"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        result = "computer wins!"
+    } else {
+        result = "it's a draw guys!";
+    }
+    return result;
+}
+
+console.log(playOneRound("scissors", getComputerChoice()));
+console.log(playOneRound("scissors", getComputerChoice()));
+console.log(playOneRound("scissors", getComputerChoice()));
+console.log(playOneRound("scissors", getComputerChoice()));
+console.log(playOneRound("scissors", getComputerChoice()));
